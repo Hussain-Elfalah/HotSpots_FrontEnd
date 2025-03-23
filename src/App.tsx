844c3b9a853da22a0851155f5e3f83c2451ca4cb
Hussain git config --global user.email <you@example.com>
2025-03-23 16:59:06 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Explore from './pages/Explore';
 import SpotDetail from './pages/SpotDetail';
